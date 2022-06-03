8563bb2358cc472df973c81bbaab495054ea1f0e
DELL <DELL@SYSTEM>
2022-06-03 12:05:00 +0400

    commit

---
+++ corejava/src/Day3_p1.java
@@ -0,0 +1,26 @@
+
+public class Day3_p1 
+{
+	int year_ofpurchase;
+	String make;
+	int model;
+	double cost;
+	public void disply1()
+	{
+		System.out.println("year of purchase"+2001);
+		System.out.println("make"+"suzuki");
+	}
+	public void disply2()
+	{
+		System.out.println("model"+1999);
+		System.out.println("cost"+20000);
+	}
+	public static void main(String[] args) {
+		Day3_p1 ob1=new Day3_p1();
+		ob1.disply1();
+		ob1.disply2();
+	}	
+		
+	}
+		
+
